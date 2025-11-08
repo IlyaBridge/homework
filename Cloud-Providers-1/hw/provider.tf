3d8233f3d5572bb3045afd6cf5310c1f8073d046
Ilya Kaznacheev <71899768+IlyaBridge@users.noreply.github.com>
2025-11-09 00:09:40 +0300

    Update provider.tf

--- Cloud-Providers-1/hw/provider.tf
+++ Cloud-Providers-1/hw/provider.tf
@@ -7,8 +7,8 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = "/home/ilya/service_account_key_file.json"
-  cloud_id  = "b1gpupamkrr85nd1d31m"
-  folder_id = "b1grpedldfrumqsrjf62"
+  service_account_key_file = var.service_account_key_path
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
   zone      = "ru-central1-a"
 }
